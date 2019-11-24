2f09694af301a7abcce062b78d427ff0fff93c1d
yuyancheng <yyc22@qq.com>
2019-11-24 09:57:57 +0800

    'gx'

--- lang/java/src/basic/langs/dt/DataTypes.java
+++ lang/java/src/basic/langs/dt/DataTypes.java
@@ -5,5 +5,17 @@ public class DataTypes {
     public static void main(String[] args) {
 
         System.out.printf("char default vlaue:" + String.valueOf(c));
+        System.out.println(c);
+        new DataTypes().test();
+    }
+
+    private void test() {
+        short s = 1;
+        s = (short)(s + 1);
+        short s2 = 1;
+        s2 += 1;
+        System.out.println(s);
+        System.out.println("s: " + String.valueOf(s));
+        System.out.println("s2: " + String.valueOf(s2));
     }
 }
