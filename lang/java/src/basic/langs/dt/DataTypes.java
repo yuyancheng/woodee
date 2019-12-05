be9b8949de65446bb57f697c3c1f250fb8a6283e
yuyancheng <yyc22@qq.com>
2019-12-05 10:54:16 +0800

    'gx'

--- lang/java/src/basic/langs/dt/DataTypes.java
+++ lang/java/src/basic/langs/dt/DataTypes.java
@@ -2,6 +2,7 @@ package basic.langs.dt;
 
 public class DataTypes {
     private static char c;
+    private int vi;
     public static void main(String[] args) {
 
         System.out.printf("char default vlaue:" + String.valueOf(c));
@@ -11,10 +12,14 @@ public class DataTypes {
 
     private void test() {
         short s = 1;
+        String s1 = "1";
+        String ss = s1 + "times";
+        //Integer v = 2;
+        System.out.println("1times" == ss);
         s = (short)(s + 1);
         short s2 = 1;
         s2 += 1;
-        System.out.println(s);
+        //System.out.println(v.equals(2));
         System.out.println("s: " + String.valueOf(s));
         System.out.println("s2: " + String.valueOf(s2));
     }
---
+++ service/spring/nix/src/main/java/com/wissy/pojo/User.java
@@ -0,0 +1,40 @@
+package com.wissy.pojo;
+
+public class User {
+    private int uid;
+    private String uname;
+    private int uage;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public int getUage() {
+        return uage;
+    }
+
+    public void setUage(int uage) {
+        this.uage = uage;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid=" + uid +
+                ", uname='" + uname + '\'' +
+                ", uage=" + uage +
+                '}';
+    }
+}
