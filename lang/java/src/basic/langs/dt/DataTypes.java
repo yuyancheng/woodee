3f4b532b6be6d751aa03946e1884b3c02a19f3c3
yuyancheng <yyc22@qq.com>
2020-01-14 23:27:04 +0800

    gx

--- lang/java/src/basic/langs/dt/DataTypes.java
+++ lang/java/src/basic/langs/dt/DataTypes.java
@@ -1,13 +1,17 @@
 package basic.langs.dt;
 
+import java.math.BigDecimal;
+
 public class DataTypes {
     private static char c;
     private int vi;
     public static void main(String[] args) {
 
-        System.out.printf("char default vlaue:" + String.valueOf(c));
-        System.out.println(c);
-        new DataTypes().test();
+        DataTypes dt = new DataTypes();
+        //dt.test();
+
+        //dt.charTest();
+        dt.floatTest();
     }
 
     private void test() {
@@ -20,7 +24,33 @@ public class DataTypes {
         short s2 = 1;
         s2 += 1;
         //System.out.println(v.equals(2));
-        System.out.println("s: " + String.valueOf(s));
-        System.out.println("s2: " + String.valueOf(s2));
+        //System.out.println("s: " + String.valueOf(s));
+        //System.out.println("s2: " + String.valueOf(s2));
+
+
+    }
+
+    private void charTest() {
+        char a = 'a';
+        int b = 010;
+        int c = 12;
+
+        System.out.printf("char default vlaue:" + String.valueOf(c));
+        System.out.println(c);
+        System.out.println(new Integer(97).equals(a));
+    }
+
+    private void floatTest() {
+        float a = 10.f;
+        double b = 2.;
+        float c = (float)(a * b);
+        double d = 20;
+        BigDecimal bd = BigDecimal.valueOf(b);
+
+        System.out.println(a * b == c);
+        System.out.println(a * b == d);
+        System.out.println(c == d);
+
+        System.out.println(bd.compareTo((BigDecimal.valueOf(b))));
     }
 }
